382bb58759bedaef8c14cb692b2b456785dbcab3
arnoldkk <kalemarnld@gmail.com>
2023-04-11 08:30:49 +0300

    added seeds

--- app/Http/Controllers/FrontEndPagesController.php
+++ app/Http/Controllers/FrontEndPagesController.php
@@ -12,28 +12,24 @@ class FrontEndPagesController extends Controller
     public function homePage()
     {
         $homePageContent = HomePage::first();
-        // HomePage::create();
         return view('home.frontend.index', compact('homePageContent'));
     }
 
     public function scholarshipPage()
     {
         $scholarshipPageContent = ScholarshipPage::first();
-        // ScholarshipPage::create();
         return view('home.frontend.scholarships', compact('scholarshipPageContent'));
     }
 
     public function aboutPage()
     {
         $aboutPageContent = AboutPage::first();
-        // AboutPage::create();
         return view('home.frontend.about', compact('aboutPageContent'));
     }
 
     public function testimonialsPage()
     {
         $testimonialsPageContent = TestimonialsPage::first();
-        // TestimonialsPage::create();
         return view('home.frontend.testimonials', compact('testimonialsPageContent'));
     }
 
