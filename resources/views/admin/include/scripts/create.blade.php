d74e36b40f14494478d779ed18969e314a840897
tundachef <rwsenfuka@gmail.com>
2023-04-21 19:50:57 +0300

    Session notifications & errors

--- resources/views/admin/include/scripts/create.blade.php
+++ resources/views/admin/include/scripts/create.blade.php
@@ -15,6 +15,63 @@
 <script src={{ asset("assets/plugins/p-scroll/perfect-scrollbar.js") }}></script>
 <script src={{ asset("assets/plugins/p-scroll/pscroll.js") }}></script>
 
+	<!-- INTERNAL Notifications js -->
+    <script src={{ asset("assets/plugins/notify/js/rainbow.js") }}></script>
+    {{-- <script src="../assets/plugins/notify/js/sample.js"></script> --}}
+    <script>
+
+        (function () {
+            $(function () {
+                @if (session('success'))
+                    $.growl.notice({
+                        message: {{ session('success') }}
+                    });
+                @endif
+
+                @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                        $.growl.error({
+                            message: $error
+                        });
+                    @endforeach
+                @endif
+
+
+              // $.growl({
+              //   title: "Growl",
+              //   message: "Hi I'm Noa!"
+              // });
+              // $.growl.notice({
+              //   message: "You have 4 notification"
+              // });
+          
+               $('.error').click(function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                return $.growl.error({
+                  message: "please check Your details ...file is missing"
+                });
+              });
+              $('.notice').click(function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                return $.growl.notice({
+                  message: "You have 4 notification"
+                });
+              });
+              return $('.warning').click(function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                return $.growl.warning({
+                  message: "read all details carefully"
+                });
+              });
+            });
+          }).call(this);
+    </script>
+    <script src={{ asset("assets/plugins/notify/js/jquery.growl.js") }}></script>
+    <script src={{ asset("assets/plugins/notify/js/notifIt.js") }}></script>
+
 <!-- bootstrap-datepicker js -->
 <script src={{ asset("assets/plugins/bootstrap-datepicker/js/datepicker.js") }}></script>
 
--- resources/views/user/include/scripts.blade.php
+++ resources/views/user/include/scripts.blade.php
@@ -12,6 +12,64 @@
 <script src={{ asset("assets/plugins/p-scroll/perfect-scrollbar.js") }}></script>
 <script src={{ asset("assets/plugins/p-scroll/pscroll.js") }}></script>
 
+	<!-- INTERNAL Notifications js -->
+    <script src={{ asset("assets/plugins/notify/js/rainbow.js") }}></script>
+    {{-- <script src="../assets/plugins/notify/js/sample.js"></script> --}}
+    <script>
+
+        (function () {
+            $(function () {
+                @if (session('success'))
+                    $.growl.notice({
+                        message: {{ session('success') }}
+                    });
+                @endif
+
+                @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                        $.growl.error({
+                            message: $error
+                        });
+                    @endforeach
+                @endif
+
+
+              // $.growl({
+              //   title: "Growl",
+              //   message: "Hi I'm Noa!"
+              // });
+              // $.growl.notice({
+              //   message: "You have 4 notification"
+              // });
+          
+               $('.error').click(function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                return $.growl.error({
+                  message: "please check Your details ...file is missing"
+                });
+              });
+              $('.notice').click(function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                return $.growl.notice({
+                  message: "You have 4 notification"
+                });
+              });
+              return $('.warning').click(function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                return $.growl.warning({
+                  message: "read all details carefully"
+                });
+              });
+            });
+          }).call(this);
+    </script>
+    <script src={{ asset("assets/plugins/notify/js/jquery.growl.js") }}></script>
+    <script src={{ asset("assets/plugins/notify/js/notifIt.js") }}></script>
+
+
 <!-- STICKY JS -->
 <script src={{ asset("assets/js/sticky.js") }}></script>
 
