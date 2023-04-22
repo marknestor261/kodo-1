184cfd56bc4c5a6cdf8407c3e423ba9fd031e01d
tundachef <rwsenfuka@gmail.com>
2023-04-22 10:32:11 +0300

    Jobs model, migration and functions in adminController done

---
+++ database/migrations/2023_04_22_071714_create_jobs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('published')->default(1);
+            $table->text('title');
+            $table->text('about');
+            $table->text('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
