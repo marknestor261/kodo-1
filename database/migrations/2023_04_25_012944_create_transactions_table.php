6d645fbc12446770af083debd94f8546e8146a03
tundachef <rwsenfuka@gmail.com>
2023-04-25 05:14:44 +0300

    Transactions & Users data, displaying

---
+++ database/migrations/2023_04_25_012944_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained;
+            $table->text('plan_interval');
+            $table->timestamp('end_at');
+            $table->text('amount');
+            $table->text('method');
+            $table->boolean('success')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
